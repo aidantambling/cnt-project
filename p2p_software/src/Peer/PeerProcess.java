af88872be984d03202d28786ea51541b8215c0c9
FredderickB <92876631+FredderickB@users.noreply.github.com>
2024-03-24 21:06:01 -0400

    first implementation of peerProcess

--- p2p_software/src/Peer/PeerProcess.java
+++ p2p_software/src/Peer/PeerProcess.java
@@ -1,6 +1,9 @@
 package Peer;
 
+import java.util.ArrayList;
+
 public class PeerProcess {
+
     public static void main(String[] args) {
         /*this is the main peerProcess, the center of the program. 
         from here the we call all the other files to get the program running
@@ -13,5 +16,42 @@ public class PeerProcess {
          *      if has file, it will only be a server
          *      if not, it will be client and server
          */
+         //for now this is hardcoded dummy implementation of peer process
+
+        // Simulate parsing a configuration 
+        String fileName = "thefile";
+        int fileSize = 167705; 
+        int pieceSize = 16384; 
+        int numberOfPieces = (int) Math.ceil(fileSize / pieceSize);
+
+        //simulate parsing peer configuration
+        int peerId = 1;
+        int port = 7000; 
+        boolean hasCompleteFile = false;
+
+        // Initialize data structures 
+
+        ArrayList<peer> peers = new ArrayList<peer>();
+        byte[] bitField = {0, 0, 0, 0, 0, 0, 0};
+
+
+        // Start logger with a dummy message 
+        System.out.println("Starting logger for peer " + peerId);
+
+        if (hasCompleteFile) {
+        
+            // peer with complete file will just serve
+            System.out.println("Peer " + peerId + " with complete file listening on port " + port + " for incoming connections.");
+
+        } else { //this peer does not have file so it will be server and client
+
+            // Simulate server
+            System.out.println("Peer " + peerId + " listening on port " + port + " for incoming connections.");
+
+            // Simulate client
+            System.out.println("Peer " + peerId + " attempting to connect to other peers...");
+
+        }
+
     }
 }
--- p2p_software/src/Peer/peer.java
+++ p2p_software/src/Peer/peer.java
@@ -1,4 +1,43 @@
 package Peer;
+import java.net.Socket;
+
 public class peer {
-  // contains all the member variables of a peer 
+  // contains all the member variables of a peer
+  private int PeerId;
+  private Socket socket;
+  private boolean interest;
+  private byte[] bitfield;
+
+  public int getPeerId() {
+    return PeerId;
+  }
+
+  public void setPeerID(int PeerId) {
+     this.PeerId = PeerId;
+  }
+
+  public Socket getSocket(){
+    return socket;
+  }
+
+  public void setSocket(Socket socket){
+    this.socket = socket;
+  }
+
+  public boolean isInterested() {
+		return interest;
+	}
+
+	public void setInterest(boolean interest) {
+		this.interest = interest;
+	}
+
+  public byte[] getBitField() {
+    return bitfield;
+  }
+  
+  public void setBitField(byte[] bitfield) {
+    this.bitfield = bitfield;
+  }
+
 }
