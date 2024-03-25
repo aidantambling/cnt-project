0e36f37c719bd1179d50123a1b93f21e7b1d9cb2
FredderickB <92876631+FredderickB@users.noreply.github.com>
2024-03-24 23:53:30 -0400

    added logger

---
+++ p2p_software/src/FileManager/Logger.java
@@ -0,0 +1,14 @@
+package FileManager;
+
+import java.io.File;
+
+public class Logger {
+    /*logger will log all the messages of a peer user. its functions
+     * its functions will each represent a type of message to be logged,
+     * predetermined in the project description.
+     */
+
+     private int peerId;
+    
+     
+}
